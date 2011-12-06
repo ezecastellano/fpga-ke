103245b0a01dd12ca4ef26419826afe1ed51b227
karinagb8 <karinagb8@50fa20cf-1811-4d89-1a5a-5b3c48f2ef7a>
2011-12-06 23:22:35 +0000

    Hicimos la fifo que nos dijo mati.

---
+++ fpga/procesadorDeImagenesFunciona/ProcesadorDeImagenes.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:15:03 11/25/2011 
+// Design Name: 
+// Module Name:    ProcesadorDeImagenes 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ProcesadorDeImagenes(
+
+    input wire clk, reset,
+    input wire rx,
+	input wire btn,
+    output wire tx,
+    output wire [3:0] an,
+    output wire [7:0] sseg, led
+   );
+
+   // signal declaration
+   wire tx_full, rx_empty, btn_tick;
+   wire [7:0] rec_data, rec_data1;
+
+   // body
+   // instantiate uart
+   uart uart_unit
+      (.clk(clk), .reset(reset), .rd_uart(signal),
+       .wr_uart(signal), .rx(rx), .w_data(rec_data1),
+       .tx_full(tx_full), .rx_empty(rx_empty), 
+       .r_data(rec_data), .tx(tx));
+   // instantiate debounce circuit
+
+	sincro sincronizador (.reset(reset), .clk(clk), rx_empty(rx_empty), .btn_tick(btn_tick), .signal(signal)); 
+
+	debounce btn_db_unit
+      (.clk(clk), .reset(reset), .sw(btn),
+       .db_level(), .db_tick(btn_tick));
+
+
+  // incremented data loops back
+   assign rec_data1 = rec_data + 50;
+   // LED display
+   assign led = rec_data;
+   assign an = 4'b1110;
+   assign sseg = {1'b1, ~tx_full, 2'b11, ~rx_empty, 3'b111};
+
+endmodule
---
+++ fpga/procesadorDeImagenesFunciona/sincro.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:26:34 12/06/2011 
+// Design Name: 
+// Module Name:    sincro 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module sincro(
+    input wire clk, reset,
+    input wire rx_empty,
+    input wire btn_tick,
+    output wire signal
+    );
+	localparam [1:0]
+	esperando = 2'b00,
+	en_proceso = 2'b10,
+	sacando_uno = 2'b01;
+	
+	// signal declaration
+	reg [1:0] state_reg, state_next;
+
+	// state register
+	always @(posedge clk, posedge reset)
+		if (reset)
+			state_reg <= esperando;
+		else
+			state_reg <= state_next;
+
+	always @* 
+		begin 
+			state_next = state_reg; // default state: the same 
+			case (state_reg) 
+				esperando: 
+					begin
+						if (btn_tick) 
+							state_next = en_proceso;
+					end
+				en_proceso: 
+					begin
+						if (rx_empty) 
+							state_next = esperando;
+						else
+							state_next = sacando_uno;
+					end
+				sacando_uno: 
+					begin
+						state_next = en_proceso; 				
+					end
+				default: 
+					state_next = esperando; 
+			endcase 
+		end 
+
+		
+	assign signal = state_reg[0];
+
+	
+endmodule
